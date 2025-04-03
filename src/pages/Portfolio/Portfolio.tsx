d7e5a1a87c98e947934646aa82f4f15c5014f10a
Zachary Melancon <zmelan01@gmail.com>
2025-04-03 14:42:25 -0600

    add deploy action

--- src/pages/Portfolio/Portfolio.tsx
+++ src/pages/Portfolio/Portfolio.tsx
@@ -50,7 +50,11 @@ export default function Portfolio() {
                 <Typography variant="body2" sx={{ my: 1 }}>
                   {project.description}
                 </Typography>
-                <Button variant="contained" href={project.link} target="_blank">
+                <Button
+                  variant="contained"
+                  href={project.link}
+                  target="_blank"
+                  rel="noopener noreferrer">
                   View
                 </Button>
               </CardContent>
@@ -62,13 +66,19 @@ export default function Portfolio() {
       <Typography variant="h4" sx={{ mt: 5, mb: 3 }}>
         Connect
       </Typography>
-      <Button variant="outlined" href="https://github.com/melanconz">
+      <Button
+        variant="outlined"
+        href="https://github.com/melanconz"
+        target="_blank"
+        rel="noopener noreferrer">
         GitHub
       </Button>
       <Button
         variant="outlined"
         href="https://linkedin.com/in/zacharymelancon"
-        sx={{ ml: 2 }}>
+        sx={{ ml: 2 }}
+        target="_blank"
+        rel="noopener noreferrer">
         LinkedIn
       </Button>
     </Container>
